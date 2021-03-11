d87f3177053a330259bbe29e2fc2ed0cc30dfdfc
Olaf-2137 <79452673+Olaf-2137@users.noreply.github.com>
2021-03-11 16:12:19 +0100

    Create Main.java

---
+++ zadanie 1/Main.java
@@ -0,0 +1,28 @@
+package sample;
+import java.util.Scanner;
+
+public class main {
+    public static void Main(String[] args) {
+    
+      //scanery zmiennych imie nazwisko rok
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj imię");
+            String name = scan.nextLine();
+            System.out.println("Podaj nazwisko");
+            String last_name = scan.nextLine();
+            System.out.println("Podaj rok urodzenia");
+            int birth_year = scan.nextInt();
+      //konto person
+            try {
+                Person p = new Person(name, last_name, birth_year);
+                System.out.println(p);
+                break;
+      //brak / zły argument
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                
+            }
+        }
+    }
+}
